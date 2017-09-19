d6bb7bb99f7df8bff0e7afad4b68ab0356ca15b8
nico24687 <theonlynico@hotmail.com>
2017-09-19 17:18:56 +0000

    created the basic functionality to display titles and view a post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,10 @@ class ArticlesController < ApplicationController
     end 
     #so the above tells rails when we go to www.localhost300.com/articles we want to do execute the indented code
     #notice articles has to be an instance variable. We want it to be visible from both the controller and the view
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    #the above is able to deal with a user clicking on a specific blog title
+    
 end
