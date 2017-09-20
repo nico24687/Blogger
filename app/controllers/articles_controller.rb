ef467b63aa60ee8616597ae72333702b9b950b19
nico24687 <theonlynico@hotmail.com>
2017-09-20 17:10:29 +0000

    added the delete workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+    
+    include ArticlesHelper
+    #the above just means we now have access to any of the methods we created in articles_helper.rb
+    #we need access to the article_params method to work around the security issue when saving content from the params hash
+    
     def index
         @articles = Article.all
     end 
@@ -9,5 +14,35 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     #the above is able to deal with a user clicking on a specific blog title
+    
+    
+    #when we clicked new article we get an error because the router went looking for an action named new inside the articles controller 
+    #but it could not find one 
+    #so we are going to create this action below 
+    
+    def new 
+        @article = Article.new
+    end
+    #so the above solves the unknonw action error but now we get an error saying there is no template
+    #we added a template new.html.erb and put an h1 in it to solve the error 
+    #note the name of the tamplate matches the name of the above controller method
+    #notice that we had to create an article in this action to deal with the fact that we have form_for(@article) in new.html.erb
+    
+    def create 
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_path(@article)
+    end 
+    #this action above helps us deal with actually creating the new post i.e. when we hit the submit button 
+    #notice by using the key word fail you can get the value of the cotent in the params hash from the form data 
+    #for security reasons it is not a good idea save parameters sent into us via the params hash. To deal with this you have to use strong parameters
+    #we do this work around in articles_helper.rb using the require method and the permit method to say its okay to save via the params hash
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+    
     
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
+    #the above gets round the fact that rails does not really like it if you save parameters sent into us via the params hash
 end
