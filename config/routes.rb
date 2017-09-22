8b78c4a8b285f339eb15cba38793e8f2a32e7842
nico24687 <theonlynico@hotmail.com>
2017-09-22 13:54:23 +0000

    finished version 1 with root page showing index and centered alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
   resources :articles
   #so here we are setting up the router of the application. We are telling rails that we want the articles model to respond to restful routing
 end
