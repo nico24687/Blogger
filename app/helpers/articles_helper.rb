81f3e78659ccafcdf3fc307581c66de59e579beb
nico24687 <theonlynico@hotmail.com>
2017-09-22 17:55:22 +0000

    need to return to the Article#tag_list method

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
     def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :tag_list)
     end
     #the above gets round the fact that rails does not really like it if you save parameters sent into us via the params hash
 end
